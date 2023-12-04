92432580b1cb03c20f40107fdea2f394b61a47be
unknown <coblexdevelopment@gmail.com>
2023-12-03 20:26:30 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {React} from "react";
+import {Introduction} from "./components/introduction";
 function App() {
+  const [menuChoice, setMenuChoice] = "intro";
+  function menuGeneration(choice){
+    switch(choice){
+      case 'intro':
+        return <Introduction />
+      case 'tutorial':
+        return <Tutorial />
+      case 'GDP':
+        return <GDPTable />
+      default:
+        console.log("MenuGeneration error");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "app-menu">
+        <h1>Data Visualization Applications</h1>
+        <div className = "app-menu-selection">
+          <button onClick={()=>setMenuChoice('intro')}>Home</button>
+          <button onClick={()=>setMenuChoice('tutorial')}>Tutorial</button>
+          <button onClick={()=>setMenuChoice('GDP')}> GDP Bar Graph </button>
+        </div>
+        <div className = "app-menu-content">
+          {menuGeneration(menuChoice)}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/introduction.js
@@ -0,0 +1,8 @@
+
+export default function introduction(){
+    return(
+        <div className = "introduction-content">
+            <p>This is the opening page to a series of projects showcasing data manipulation and display. Tutorial is the results of a guided tutorial from TopCoder's academy. The GDP Table is one of the independent projects assigned by the course.</p>
+        </div>
+    );
+}
